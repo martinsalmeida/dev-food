29f26fbd7b794dba9efe93698dc4a2fd064f0774
Daniel Almeida <daniel.arquiteto@gmail.com>
2024-05-21 12:52:52 -0300

    feat: add button card to product page

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -2,6 +2,7 @@ import { db } from '@/app/_lib/prisma';
 import { notFound } from 'next/navigation';
 import ProductImage from './_components/product-image';
 import ProductDetails from './_components/product-details';
+import { Button } from '@/app/_components/ui/button';
 
 interface ProductPageProps {
   params: {
@@ -43,6 +44,10 @@ const ProductPage = async ({ params: { id } }: ProductPageProps) => {
         product={JSON.parse(JSON.stringify(product))}
         complementaryProduct={JSON.parse(JSON.stringify(juices))}
       />
+
+      <div className="mt-6 mb-6 px-5">
+        <Button className="w-full font-semibold">Adicionar a sacola</Button>
+      </div>
     </div>
   );
 };
