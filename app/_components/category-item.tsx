4b95f6d2d5c0110eec95597865bb6372d21265ea
Daniel Almeida <daniel.arquiteto@gmail.com>
2024-05-25 17:58:45 -0300

    feat: add category page

--- app/_components/category-item.tsx
+++ app/_components/category-item.tsx
@@ -1,5 +1,6 @@
 import { Category } from '@prisma/client';
 import Image from 'next/image';
+import Link from 'next/link';
 
 interface CategoryItemProps {
   category: Category;
@@ -7,15 +8,17 @@ interface CategoryItemProps {
 
 const CategoryItem = ({ category }: CategoryItemProps) => {
   return (
-    <div className="flex justify-center items-center gap-3 py-3 px-8 bg-white rounded-full shadow-md">
-      <Image
-        src={category.imageUrl}
-        alt={category.name}
-        width={30}
-        height={30}
-      />
-      <span className="font-semibold text-sm">{category.name}</span>
-    </div>
+    <Link href={`/categories/${category.id}/products`}>
+      <div className="flex justify-center items-center h-14 mb-3 py-3 px-8 gap-2 bg-white rounded-full shadow-md">
+        <Image
+          src={category.imageUrl}
+          alt={category.name}
+          width={30}
+          height={30}
+        />
+        <span className="font-semibold text-sm">{category.name}</span>
+      </div>
+    </Link>
   );
 };
 
--- app/_components/category-list.tsx
+++ app/_components/category-list.tsx
@@ -5,7 +5,7 @@ const CategoryList = async () => {
   const categories = await db.category.findMany({});
 
   return (
-    <div className="flex overflow-x-scroll  gap-3 [&::-webkit-scrollbar]:hidden">
+    <div className="flex overflow-x-scroll gap-3 [&::-webkit-scrollbar]:hidden">
       {categories.map((category) => (
         <CategoryItem key={category.id} category={category} />
       ))}
